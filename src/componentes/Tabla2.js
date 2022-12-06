507972c24becf7b71e7c26449bd7067ef0322da5
JhonSandoval <69171854+JhonSandoval@users.noreply.github.com>
2022-12-05 19:25:50 -0500

    arreglado

--- src/componentes/Tabla2.js
+++ src/componentes/Tabla2.js
@@ -8,61 +8,50 @@ export default class Datos extends PureComponent {
 
         return (
 
+            <div className="container mt-5" align="center">
 
-            <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-                <div class="col">
-                    <div class="card mb-4 rounded-3 shadow-sm">
-                        <div class="card-header py-3">
-                            <h4 class="my-0 fw-normal">{data.title}</h4>
-                        </div>
-                        <div class="card-body">
-                            <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
-                            <ul class="list-unstyled mt-3 mb-4">
-                                <li>10 users included</li>
-                                <li>2 GB of storage</li>
-                                <li>Email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
-                        </div>
+                <h4>Lista de Precios</h4>
+                <br />
+
+                <div className="row">
+
+                    <div className="col-md-12">
+
+                        <table className="table table-bordered">
+                            <thead className="thead-dark">
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Stock</th>
+                                    <th scope="col">Precio</th>
+                                    <th scope="col">Imagen</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                {data.map(item => (
+
+                                    <tr key={item.id}>
+                                        <td>{item.id}</td>
+                                        <td>{item.title}</td>
+                                        <td>{item.content}</td>
+                                        <td>{item.precio}</td>
+                                        <td>{item.img}</td>
+                                    {/**    <td><img src={src="https://picsum.photos/800/600?random=4"} /></td>
+                                   */} </tr>
+
+                                ))}
+
+                            </tbody>
+
+                        </table>
+
                     </div>
+
                 </div>
-                <div class="col">
-                    <div class="card mb-4 rounded-3 shadow-sm">
-                        <div class="card-header py-3">
-                            <h4 class="my-0 fw-normal">Pro</h4>
-                        </div>
-                        <div class="card-body">
-                            <h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
-                            <ul class="list-unstyled mt-3 mb-4">
-                                <li>20 users included</li>
-                                <li>10 GB of storage</li>
-                                <li>Priority email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card mb-4 rounded-3 shadow-sm border-primary">
-                        <div class="card-header py-3 text-bg-primary border-primary">
-                            <h4 class="my-0 fw-normal">Enterprise</h4>
-                        </div>
-                        <div class="card-body">
-                            <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
-                            <ul class="list-unstyled mt-3 mb-4">
-                                <li>30 users included</li>
-                                <li>15 GB of storage</li>
-                                <li>Phone and email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
-                        </div>
-                    </div>
-                </div>
+
+
             </div>
-
 
         )
     }
